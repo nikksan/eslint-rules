f82df438c500f650ffd2d7373c703db0b11889bc
Todor Petrov <todor.petrov@luckbox.com>
2020-11-12 16:42:19 +0200

    Enforce @typescript-eslint/explicit-module-boundary-types and no-only-tests/no-only-tests rules in backend

--- backend.js
+++ backend.js
@@ -4,7 +4,10 @@ module.exports = {
     "es6": true,
     "jest": true
   },
-  "plugins": ["node"],
+  "plugins": [
+    "node",
+    "no-only-tests"
+  ],
   "extends": [
     __dirname + "/common.js",
    "plugin:node/recommended"
@@ -16,6 +19,7 @@ module.exports = {
     "no-useless-catch": "off",
     "require-atomic-updates": "off",
     "no-duplicate-imports": "error",
+    "no-only-tests/no-only-tests": "error"
   },
   "overrides": [
     {
@@ -25,7 +29,7 @@ module.exports = {
         "node/no-unsupported-features/es-syntax": "off",
         "node/no-missing-require": "off",
         "node/no-unpublished-import": "off",
-        "no-undef": "off",
+        "no-undef": "off"
       }
     }
   ],
